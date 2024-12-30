670b01b8cff4a58560a2a2e1a958ceef52e7321c
mohammad <127003546+mohammadreza1408@users.noreply.github.com>
2024-12-30 12:38:59 -0500

    Gradient_descent

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -6,19 +6,29 @@ matplotlib.use('TkAgg')
 x= np.array([1,2,16,4,5]) #independent variable
 y= np.array([2,4,5,4,5]) #dependent variable
 
-N=len(x)
-
-m= (N * np.sum(x*y) - np.sum(x) * np.sum(y)) / (N* np.sum(x **2) - np.sum(x **2))
-c= (np.sum(y) - m * np.sum(x))/ N
-
-print(f"Slop (m) is : {m}")
-print(f"intercept elevation (c) is : {c}")
 
 
-y_pred = m * x +c
-plt.scatter(x,y,color='blue',label='Data')
-plt.plot(x,y_pred, color='red',label='Regretion Line')
-plt.xlabel('x')
-plt.ylabel('y')
+
+m,c =0,0
+learning_rate = 0.01
+epochs = 1000
+
+for _ in range(epochs):
+    y_pred = m * x +c
+
+    dm = (-2 / len(x)) * np.sum(x * (y- y_pred))
+    dc = (-2 / len(x)) * np.sum(y - y_pred)
+
+
+    m -= learning_rate *dm
+    c -= learning_rate * dc
+
+print(f"Slop (m): {m}")
+print(f"intercept elevation (c): {c}")
+
+plt.scatter(x, y, color="blue", label="Data")
+plt.plot(x, m * x + c, color="green", label="Gradient descent")
+plt.xlabel("X")
+plt.ylabel("y")
 plt.legend()
 plt.show()
